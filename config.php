d5037b8342d57f678be1d6ab02ca5b92c990aad4
TÉCNICO EM INFORMÁTICA <51127512022.2@senacrjedu.local>
2022-11-21 09:02:36 -0300

    auladesexta

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$conexao = new mysqli('localhost:3306', 'root' , '', 'auladesexta');
+
+
+if($conexao->connect_errno)
+{
+
+    echo "Tem alguma coisa errada";
+}
+else {
+    echo "Conexao bem sucedida";
+}
+
+?>
+
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+if(isset($POST['submit'])) {
+
+    include_once('config.php');
+
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+
+    $result = mysqli_query($conexao, "INSERT INTO cadastro(nome,telefone) values('$nome', '$telefone')");
+
+
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA DE SEXTA</title>
+    <div>
+
+    <form action="index.php" method="POST">       
+    </div>
+
+  <form>
+    <div>
+      <fieldset>
+        <legend>Formulário</legend>
+        <label>cadastrar</label>
+        <br>
+        <br>
+        <br>
+        <label>Nome</label>
+           <input type="text" name="nome" id="nome"/>
+        <br>
+        <br>
+        <label>Telefone</label>
+        <input type="number" name="telefone" id="telefone"/>
+
+        <input type="submit" name="submit" id="submit"/>
+      </fieldset>
+    </div>
+  </form>
+  
+</head>
+<body>
+    
+</body>
+</html>
